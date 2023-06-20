40ab9c79f57a7205204430a09c654b722b0eb4cd
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-06-20 09:05:25 +0300

    authentication

--- app/src/main/java/com/example/billsmanagement/MainActivity.kt
+++ app/src/main/java/com/example/billsmanagement/MainActivity.kt
@@ -15,45 +15,45 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-override fun onResume() {
-    super.onResume()
-    setContentView(binding.root)
-    binding.btnSignUp.setOnClickListener {
-        validateRegistration()
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.btnSignUp.setOnClickListener {
+            validateRegistration()
+        }
     }
-}
-fun validateRegistration() {
-    val name = binding.etusername.text.toString()
-    val phonenumber =binding.etPhoneNumber .text.toString()
-    val email=binding.etEmail .text.toString()
-    val password =binding.etPassword .text.toString()
-    val confirm=binding.etConfirm.text.toString()
-    var error = false
-    if (name.isBlank()) {
-        binding.tilUsername.error = "Name required"
-        error=true
-    }
-    if (name.isBlank()) {
-        binding.tilPhoneNumber.error = "Phonenumber required"
-        error=true
-    }
-    if (name.isBlank()) {
-        binding.tilEmail.error = "Email required"
-        error=true
-    }
+    fun validateRegistration() {
+        val name = binding.etusername.text.toString()
+        val phonenumber =binding.etPhoneNumber .text.toString()
+        val email=binding.etEmail .text.toString()
+        val password =binding.etPassword .text.toString()
+        val confirm=binding.etConfirm.text.toString()
+        var error = false
+        if (name.isBlank()) {
+            binding.tilUsername.error = "Name required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tilPhoneNumber.error = "Phonenumber required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tilEmail.error = "Email required"
+            error=true
+        }
 
-    if (name.isBlank()) {
-        binding.tilPassword.error = "Password required"
-        error=true
+        if (name.isBlank()) {
+            binding.tilPassword.error = "Password required"
+            error=true
+        }
+        if (confirm!=password){
+            binding.tilConfirm.error="Try again"
+            error=true
+        }
+        if (!error){
+            val intent= Intent(this,Login::class.java)
+            startActivity(intent)
+        }
     }
-    if (confirm!=password){
-        binding.tilConfirm.error="Try again"
-        error=true
-    }
-    if (!error){
-        val intent= Intent(this,MainActivity::class.java)
-        startActivity(intent)
-    }
-}
 }
 
