b62acc9ab9970d55b70fedd76e29629ebc318ca0
Jemima Nyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-08-01 07:13:38 +0300

    commits

---
+++ app/src/main/java/com/example/billsmanagement/model/LoginResponse.kt
@@ -0,0 +1,10 @@
+package com.example.billsmanagement.model
+
+data class LoginResponse(
+var message:String,
+//var message:String,
+var user:User
+//var user_id:String,
+//var access_token:String
+
+)
---
+++ app/src/main/java/com/example/billsmanagement/ui/SplashScreen.kt
@@ -0,0 +1,95 @@
+package com.example.billsmanagement.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.billsmanagement.R
+import com.example.billsmanagement.databinding.ActivitySplashScreenBinding
+import com.example.billsmanagement.model.RegisterRequest
+import com.example.billsmanagement.viewmodel.UserViewModel
+
+class SplashScreen : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
+    val userViewModel:UserViewModel by  viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.btnSignUp.setOnClickListener {
+            validateRegistration()
+        }
+        userViewModel.errorLiveData.observe(this, Observer { err->
+            Toast.makeText(this,err,Toast.LENGTH_LONG).show()
+            binding.pbRegister.visibility=View.GONE
+            val intent= Intent(this, Login::class.java)
+            startActivity(intent)
+        })
+
+        userViewModel.regLiveData.observe(this, Observer { regResponse->
+            binding.pbRegister.visibility=View.GONE
+            Toast.makeText(this,regResponse.message,Toast.LENGTH_LONG).show()
+            val intent= Intent(this, Login::class.java)
+            startActivity(intent)
+        })
+    }
+    fun validateRegistration() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName=binding.etLastName.text.toString()
+        val phoneNumber =binding.etPhoneNumber .text.toString()
+        val email=binding.etEmail .text.toString()
+        val password =binding.etPassword .text.toString()
+        val confirm=binding.etConfirm.text.toString()
+        var error = false
+        if (firstName.isBlank()) {
+            binding.tilFirstName.error = "Name required"
+            error=true
+        }
+        if (lastName.isBlank()) {
+            binding.tilLastName.error = "Name required"
+            error=true
+        }
+        if (phoneNumber.isBlank()) {
+            binding.tilPhoneNumber.error = "Phonenumber required"
+            error=true
+        }
+        if (email.isBlank()) {
+            binding.tilEmail.error = "Email required"
+            error=true
+        }
+
+        if (password.isBlank()) {
+            binding.tilPassword.error = "Password required"
+            error=true
+        }
+        if (confirm!=password){
+            binding.tilConfirm.error="Try again"
+            error=true
+        }
+        if (!error){
+//            names parameters
+            val registerRequest=RegisterRequest(
+                firstName=firstName,
+                lastName=lastName,
+                email=email,
+                password=password,
+                phoneNumber = phoneNumber
+            )
+//            val intent= Intent(this, Login::class.java)
+//            startActivity(intent)
+            binding.pbRegister.visibility= View.VISIBLE
+            userViewModel.registerUser(registerRequest)
+
+        }
+    }
+}
+
+
